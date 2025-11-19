75cb86f81771803e138c9053e7f01f56efec229f
Parham Mo Asghari <parhammoasghari@gmail.com>
2025-11-19 15:02:01 +0330

    added jaeger for tracing

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,10 +2,13 @@ import asyncio
 import aio_pika
 import json
 import os
+from tracing import setup_tracer
+from opentelemetry.propagate import extract
 
 RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
 QUEUE_NAME = "task_queue"
 
+tracer = setup_tracer("rabbitmq-consumer")
 
 async def connect_rabbit():
     while True:
@@ -17,34 +20,28 @@ async def connect_rabbit():
             print(f"RabbitMQ not ready, retrying in 3s... ({e})")
             await asyncio.sleep(3)
 
-
 async def process_message(message: aio_pika.abc.AbstractIncomingMessage):
     async with message.process():
         data = json.loads(message.body.decode())
-        print("Received:", data)
 
-        # Simulate async processing
-        await asyncio.sleep(6)
+        # Extract trace context from message headers
+        ctx = extract(message.headers or {})
 
-        print("Processed:", data)
-
+        with tracer.start_as_current_span("process_message", context=ctx) as span:
+            span.set_attribute("message.id", data.get("id"))
+            span.set_attribute("message.content", data.get("msg"))
+            print("Received:", data)
+            await asyncio.sleep(2)  # simulate processing
+            print("Processed:", data)
 
 async def main():
     connection = await connect_rabbit()
-
     async with connection:
         channel = await connection.channel()
         await channel.set_qos(prefetch_count=1)
-
         queue = await channel.declare_queue(QUEUE_NAME, durable=True)
-
-        print(" [*] Waiting for messages …")
-
         await queue.consume(process_message)
-
-        # Keep the consumer running forever
-        await asyncio.Future()
-
+        await asyncio.Future()  # run forever
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ consumer/tracing.py
@@ -0,0 +1,18 @@
+# tracing.py
+from opentelemetry import trace
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+
+def setup_tracer(service_name: str):
+    resource = Resource.create({"service.name": service_name})
+    provider = TracerProvider(resource=resource)
+
+    processor = BatchSpanProcessor(
+        OTLPSpanExporter(endpoint="http://jaeger:4317", insecure=True)
+    )
+    provider.add_span_processor(processor)
+
+    trace.set_tracer_provider(provider)
+    return trace.get_tracer(service_name)
